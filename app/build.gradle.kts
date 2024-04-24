2f29d1c1f0d3b9e794e78bfbb8f10ad5a8f86437
William Ibarra <wa_ibarra@hotmail.com>
2024-04-24 11:49:52 -0600

    [Added] Navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,7 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
