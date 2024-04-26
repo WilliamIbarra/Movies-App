5ef2cf759c557dcf7a23b67704cf576274b4dd52
William Ibarra <wa_ibarra@hotmail.com>
2024-04-26 16:40:39 -0600

    [Added] Called to NavHost in the main activity to use the navigation graph

--- app/src/main/java/com/example/moviesapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/MainActivity.kt
@@ -6,10 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.moviesapp.navigation.AppNavHost
 import com.example.moviesapp.ui.theme.MoviesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    // Creates a NavHostController
+                    AppNavHost(navController = rememberNavController())
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MoviesAppPreview() {
     MoviesAppTheme {
-        Greeting("Android")
+        AppNavHost(navController = rememberNavController())
     }
 }
