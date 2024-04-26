7d050fe3c2ba36a4b2fad8bbec4dcfcb72ffad42
William Ibarra <wa_ibarra@hotmail.com>
2024-04-26 16:29:56 -0600

    [Added] Content for Detail and Home screens, NavHost controller to navigate and Movie object

---
+++ app/src/main/java/com/example/moviesapp/data/Movie.kt
@@ -0,0 +1,6 @@
+package com.example.moviesapp.data
+
+data class Movie( // Object to save movie information
+    val drawable: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/example/moviesapp/navigation/AppNavHost.kt
@@ -0,0 +1,56 @@
+package com.example.moviesapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.moviesapp.R
+import com.example.moviesapp.data.Movie
+import com.example.moviesapp.ui.screens.DetailScreen
+import com.example.moviesapp.ui.screens.HomeScreen
+import com.example.moviesapp.ui.screens.LoginScreen
+import com.example.moviesapp.utilities.NavigationItem
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier, // The modifier to be applied to the layout
+    navController: NavHostController, // The navController for this host
+    startDestination: String = NavigationItem.Login.route // Start route
+) {
+    NavHost( // Provides in place in the Compose hierarchy for self contained navigation to occur.
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        composable( // This method adds the composable to the NavGraphBuilder
+            route = NavigationItem.Login.route // Route for the destination
+        ) {
+            LoginScreen() // Composable for the destination
+        }
+
+        composable( // This method adds the composable to the NavGraphBuilder
+            route = NavigationItem.Home.route // Route for the destination
+        ) {
+            HomeScreen() // Composable for the destination
+        }
+
+        composable( // This method adds the composable to the NavGraphBuilder
+            route = NavigationItem.Detail.route, // Route for the destination
+            arguments = listOf( // List of arguments passed by navigation
+                navArgument(name="movieName") { type = NavType.StringType }, // Movie name by argument
+                navArgument(name = "movieImage") { type = NavType.ReferenceType } // Drawable reference
+            )
+        ) {
+            val image = it.arguments?.getInt("movieImage", R.drawable.no_image_available) ?: R.drawable.no_image_available // Get the image by argument
+            val name = it.arguments?.getString("movieName", "") ?: "" // Get the name by argument
+
+            // Create a new movie object
+            val movie = Movie(name = name, drawable = image)
+
+            DetailScreen(movie = movie) // Composable for the destination, this composable receives a movie object
+        }
+    }
+}
